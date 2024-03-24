5a15ef4c14047c99bca4081ad1adf0cdc0198957
Paul <paul.kwon@bunjang.co.kr>
2024-03-24 18:20:13 +0900

    chore: configure styledComponents

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import styled from '~/styledComponents'
 
 const App = () => {
   return <>
-    <div>Hello Nice to meet you.</div>
+    <AppWrapper>
+      <div>Hello Nice to meet you.</div>
+    </AppWrapper>
   </>
 }
+
+const AppWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: red;
+`;
 
 export default App;
